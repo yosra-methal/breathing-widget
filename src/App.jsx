7855e79a82a4d79acf16ff8b3957a6438a96eda6
yosra.methal <yosra.methal1@gmail.com>
2025-12-09 18:18:22 +0100

    Initial commit of breathing widget

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>Breathing Widget</h1>
+        </div>
+    )
+}
+
+export default App
